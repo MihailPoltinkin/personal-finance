7c06d4c1c86b1eebc89bb3bd00274304d4676b8e
Mihail P <70218035@online.muiv.rugit config --global user.name Mihail>
2026-05-16 16:28:54 +0300

    Initial main branch with core files

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-# main.py
 balance = 0
 transactions = []
-
-def add_income(amount):
-    global balance
-    balance += amount
-    transactions.append(('income', amount))
-
-def add_expense(amount):
-    global balance
-    balance -= amount
-    transactions.append(('expense', amount))
